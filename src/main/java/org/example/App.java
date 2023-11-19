5e6d4250fcbeaa7491f6a4cd57fa15b3d87ba815
shaon <shaonx@proton.me>
2023-11-20 00:11:34 +0600

    Refactor App class: restructure code, update formatting, and improve code comments

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -7,33 +7,38 @@ import java.sql.Date;
 
 public class App {
     public static void main(String[] args) {
-
+        String message;
         MovieDAO movieDAO = new MovieDAO();
 
         // Creating and initializing movie objects.
-        // The release date of The Shawshank Redemption movie is intentionally misspelled here.
-        Movie movie1 = new Movie("The Shawshank Redemption", "Frank Darabont", "Drama", Date.valueOf("1894-9-23"), "USA", "Tim Robbins, Morgan Freeman");
-        Movie movie2 = new Movie("The Godfather", "Francis Ford Coppola", "Crime", Date.valueOf("1972-2-24"), "USA", "Marlon Brando, Al Pacino");
-        Movie movie3 = new Movie("The Dark Knight", "Christopher Nolan", "Action", Date.valueOf("2008-7-18"), "USA", "Christian Bale, Heath Ledger");
+        // The original release date of The Shawshank Redemption movie is 1994-9-23.
+        // We will fix this later with an update.
+        Movie shawshankRedemption = new Movie(
+                "The Shawshank Redemption",
+                "Frank Darabont",
+                "Drama",
+                Date.valueOf("1984-9-23"),
+                "USA",
+                "Tim Robbins, Morgan Freeman");
 
-        // Saving movies to database.
-        String messageS1 = movieDAO.addMovie(movie1);
-        System.out.println(messageS1);
+        Movie godfather = new Movie(
+                "The Godfather",
+                "Francis Ford Coppola",
+                "Crime",
+                Date.valueOf("1972-2-24"),
+                "USA",
+                "Marlon Brando, Al Pacino");
 
-        String messageS2 = movieDAO.addMovie(movie2);
-        System.out.println(messageS2);
+        // Saving movies to database
+        message = movieDAO.addMovie(shawshankRedemption);
+        System.out.println(message);
 
-        String messageS3 = movieDAO.addMovie(movie3);
-        System.out.println(messageS3);
+        message = movieDAO.addMovie(godfather);
+        System.out.println(message);
 
-        // Updating movies in database.
-        Movie updatedMovieObject = new Movie(1,"The Shawshank Redemption", "Frank Darabont", "Drama", Date.valueOf("1994-9-23"), "USA", "Tim Robbins, Morgan Freeman");
-        String messageU = movieDAO.updateMovie(updatedMovieObject);
-        System.out.println(messageU);
 
-        // Retrieving a single movie from database.
-        // Returns null if movie not found.
-        Movie movie = movieDAO.getMovieById(1);
+        // Retrieving a single movie
+        Movie movie = movieDAO.getMovieById(1L);
 
         if (movie != null) {
             System.out.println("Title: " + movie.getTitle());
@@ -41,14 +46,27 @@ public class App {
             System.out.println("No movie found in database");
         }
 
-        // // Retrieving all movies from database.
+        // Retrieving all movies
         for (Movie m : movieDAO.getAllMovies()) {
             System.out.println("Title: " + m.getTitle());
         }
 
-        // Deleting a movie from database.
-        String messageD = movieDAO.deleteMovieById(1);
-        System.out.println(messageD);
+        // Update movie
+        Movie updatedShawshankRedemption = new Movie(
+                1L,
+                "The Shawshank Redemption",
+                "Frank Darabont",
+                "Drama",
+                Date.valueOf("1994-9-23"),
+                "USA",
+                "Tim Robbins, Morgan Freeman");
+        message = movieDAO.updateMovie(updatedShawshankRedemption);
+        System.out.println(message);
+
+
+        // Delete movie
+        message = movieDAO.deleteMovieById(2L);
+        System.out.println(message);
     }
 }
 
